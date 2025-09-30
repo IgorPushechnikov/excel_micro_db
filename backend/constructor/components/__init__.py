01647024ae319a72aae013140d4347c48c9cff3b
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-09-30 13:06:12 +0700

    Удаление устаревших файлов GUI из backend: constructor, temp_delete_empty, app_controller.py.bak, cli/cli_main.py

--- backend/constructor/components/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/constructor_new/components/__init__.py
-"""
-Компоненты графического интерфейса Excel Micro DB (новый GUI).
-Содержит виджеты, такие как SheetEditor, ProjectExplorer, NodeEditor и т.д.
-"""
-# Пока пустой, можно добавить импорты в будущем
