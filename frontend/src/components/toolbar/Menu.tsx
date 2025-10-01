984854fa1d9499b670f9428063ce9a96fae18c0e
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-02 00:23:26 +0700

    Create Menu.tsx
    Созданы заглушки основных компонентов интерфейса

---
+++ frontend/src/components/toolbar/Menu.tsx
@@ -0,0 +1,13 @@
+// frontend/src/components/toolbar/Menu.tsx
+
+import React from 'react';
+
+const Menu: React.FC = () => {
+  return (
+    <div className="text-sm font-medium">
+      Меню: Файл | Главная | Вставка | Данные | Вид | Справка
+    </div>
+  );
+};
+
+export default Menu;
