aa642c41de9853be0a113315d20429c523bce084
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-02 01:46:38 +0700

    Create index.js
    Добавлена структура YAML для меню, универсальные компоненты Menu/ContextMenu, реализованы контекстные меню в NodeEditor.tsx с обработкой действий

---
+++ frontend/src/config/context_menus/index.js
@@ -0,0 +1,12 @@
+// frontend/src/config/context_menus/index.js
+
+// Импорт конфигураций контекстных меню
+import paneMenuConfig from './node_editor_pane.yaml';
+import baseNodeMenuConfig from './node_types/base_node.yaml';
+// Здесь можно импортировать и другие конфигурации для конкретных типов узлов
+
+export {
+  paneMenuConfig,
+  baseNodeMenuConfig,
+  // ... другие конфигурации
+};
