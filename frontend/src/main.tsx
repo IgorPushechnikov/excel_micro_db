ceb568088e5776aee469ac595f9fc0be12a0104c
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-01 22:53:50 +0700

    Update main.tsx
    Установка

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; /* Импорт стилей Tailwind */
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
