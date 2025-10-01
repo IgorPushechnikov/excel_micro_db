344ac96b65bc9a2353071a9012c5fd87eea5b86a
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-02 00:22:55 +0700

    Create StatusBar.tsx
    Созданы заглушки основных компонентов интерфейса

---
+++ frontend/src/components/layout/StatusBar.tsx
@@ -0,0 +1,11 @@
+// frontend/src/components/layout/StatusBar.tsx
+
+import React from 'react';
+
+const StatusBar: React.FC = () => {
+  // StatusBar уже отображается в footer MainLayout.tsx
+  // Этот компонент можно использовать для более сложной логики статусной строки в будущем
+  return null; // Пока возвращает null, так как содержимое отображается в MainLayout
+};
+
+export default StatusBar;
