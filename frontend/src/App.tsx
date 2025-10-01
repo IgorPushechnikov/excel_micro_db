8753a27fec6a518a88729862bcbe69ba0350b466
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-01 22:53:21 +0700

    Update App.tsx
    Обновлён App.tsx: удалён импорт App.css, добавлен утилитарный класс bg-gray-100 для работы Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
+// src/App.tsx
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,7 +13,8 @@ function App() {
   }
 
   return (
-    <main className="container">
+    // Добавлен утилитарный класс bg-gray-100, чтобы Tailwind его обнаружил
+    <main className="container bg-gray-100">
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
