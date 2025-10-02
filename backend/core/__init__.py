15b2d811aff69773c3df5215c402c484b47f262a
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-03 05:35:51 +0700

    Update __init__.py
    Исправлен комментарий в backend/core/__init__.py: src -> backend

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,9 +1,9 @@
-# src/core/__init__.py
+# backend/core/__init__.py
 """
 Инициализация пакета core.
 """
 
 # Пустой файл инициализации для определения core как пакета.
 # Импорты основных классов (например, AppController) должны выполняться напрямую
-# из их модулей: from src.core.app_controller import AppController.
+# из их модулей: from backend.core.app_controller import AppController.
 
