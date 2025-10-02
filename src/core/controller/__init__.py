8d8ae0ae953e36234183f3f9ab4980b8ccbc9a39
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-03 06:14:01 +0700

    удалено

--- src/core/controller/__init__.py
+++
@@ -1,9 +0,0 @@
-# src/core/controller/__init__.py
-"""
-Модуль управления логикой приложения.
-Содержит AppController и вспомогательные менеджеры.
-"""
-# Импорты для удобства, если нужно использовать напрямую из controller
-# from .app_controller import AppController
-# from .data_manager import DataManager
-# ...
