db2b460a412506afaf72f19d7eb34508dd261903
Igor Pushechnikov <ipushechnikov@yandex.ru>
2025-10-02 19:49:49 +0700

    Update MainLayout.jsx
    проверка

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from '../toolbar/Menu';
-import Ribbon from '../toolbar/Ribbon';
-import FormulaBar from '../toolbar/FormulaBar';
-import ProjectExplorer from '../explorer/ProjectExplorer';
-import DataTable from '../table/DataTable';
-import NodeEditor from '../node-editor/NodeEditor';
-import NodeProperties from '../node-editor/NodeProperties';
-import FunctionPanel from '../toolbar/FunctionPanel';
+// import Menu from '../toolbar/Menu';
+// import Ribbon from '../toolbar/Ribbon';
+// import FormulaBar from '../toolbar/FormulaBar';
+// import ProjectExplorer from '../explorer/ProjectExplorer';
+// import DataTable from '../table/DataTable';
+// import NodeEditor from '../node-editor/NodeEditor';
+// import NodeProperties from '../node-editor/NodeProperties';
+// import FunctionPanel from '../toolbar/FunctionPanel';
 import StatusBar from './StatusBar';
 
 const MainLayout = () => {
@@ -14,16 +14,16 @@ const MainLayout = () => {
     <div className="h-screen w-screen flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-0 m-0 overflow-hidden">
       {/* Верхняя панель */}
       <div className="flex flex-col h-auto">
-        <Menu />
+        {/* <Menu />
         <Ribbon />
-        <FormulaBar />
+        <FormulaBar /> */}
       </div>
 
       {/* Центральная область */}
       <div className="flex flex-1 overflow-hidden">
         {/* Левая панель (Обозреватель проекта) */}
         <div className="w-1/4 border-r border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 flex flex-col">
-          <ProjectExplorer />
+          {/* <ProjectExplorer /> */}
         </div>
 
         {/* Правая панель (Таблица и Нодовый редактор сверху, Панель свойств снизу) */}
@@ -32,26 +32,26 @@ const MainLayout = () => {
           <div className="flex flex-1 overflow-hidden">
             {/* Таблица (2/3 ширины правой панели) */}
             <div className="flex-2 border-r border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800">
-              <DataTable />
+              {/* <DataTable /> */}
             </div>
             {/* Нодовый редактор (1/3 ширины правой панели) */}
             <div className="flex-1 bg-white dark:bg-gray-800">
-              <NodeEditor />
+              {/* <NodeEditor /> */}
             </div>
           </div>
 
           {/* Нижняя часть (Панель свойств) */}
           <div className="h-1/4 border-t border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800">
-            <NodeProperties />
+            {/* <NodeProperties /> */}
           </div>
         </div>
       </div>
 
       {/* Нижняя панель (Функции и Состояние) */}
       <div className="flex h-10 border-t border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-700">
-        <div className="w-1/3 border-r border-gray-300 dark:border-gray-600">
+        {/* <div className="w-1/3 border-r border-gray-300 dark:border-gray-600">
           <FunctionPanel />
-        </div>
+        </div> */}
         <div className="flex-1">
           <StatusBar />
         </div>
